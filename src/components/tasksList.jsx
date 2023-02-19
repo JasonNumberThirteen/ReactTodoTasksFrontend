54743ba978c3b2c77a04c1924937997647e2e62b
Jason <numberthirteen4026@gmail.com>
2023-02-19 15:29:18 +0100

    Add a JSX script for tasks list

---
+++ src/components/tasksList.jsx
@@ -0,0 +1,11 @@
+import {useEffect, useState} from "react";
+import {Link, useNavigate} from "react-router-dom";
+
+const TasksList = () =>
+{
+	return (
+		<div>
+			<h2>Lista zadań</h2>
+		</div>
+	);
+};
