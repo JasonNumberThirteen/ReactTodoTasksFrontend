f03cf2610b648a839d42a984891289e1c76658d5
Jason <numberthirteen4026@gmail.com>
2023-02-19 16:38:03 +0100

    Add the task editor as a route in the App.js script

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import {BrowserRouter, Routes, Route} from "react-router-dom";
 
 import TasksList from "./components/tasksList.jsx"
 import TaskCreator from "./components/taskCreator.jsx"
+import TaskEditor from "./components/taskEditor.jsx"
 
 function App()
 {
@@ -13,6 +14,7 @@ function App()
 				<Routes>
 					<Route path = "/" element = {<TasksList />}></Route>
 					<Route path = "/task/create" element = {<TaskCreator />}></Route>
+					<Route path = "/task/edit/:taskid" element = {<TaskEditor />}></Route>
 				</Routes>
 			</BrowserRouter>
 		</div>
