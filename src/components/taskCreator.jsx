d58e32ec67428001d0947dfcee3d4e93247ca574
Jason <numberthirteen4026@gmail.com>
2023-02-19 16:23:23 +0100

    Add callbacks in the task creator

--- src/components/taskCreator.jsx
+++ src/components/taskCreator.jsx
@@ -3,18 +3,40 @@ import {Link, useNavigate, useParams} from "react-router-dom";
 
 const TaskCreator = () =>
 {
+	const [name, changedName] = useState("");
+	const [description, changedDescription] = useState("");
+	const un = useNavigate();
+	const onNameChange = (e) => changedName(e.target.value);
+	const onDescriptionChange = (e) => changedDescription(e.target.value);
+	const onSaveClick = (e) =>
+	{
+		e.preventDefault();
+
+		const completed = false;
+		const data = {name, description, completed};
+
+		fetch("http://localhost:8080/api/tasks",
+		{
+			method: "POST",
+			headers: {"content-type": "application/json"},
+			body: JSON.stringify(data)
+		}).then(() => un("/"))
+		.catch((error) => console.log(error.message));
+	};
+	const onBackClick = () => un("/");
+	
 	return (
 		<div>
 			<h2>Dodaj nowe zadanie</h2>
-			<form>
+			<form onSubmit = {(e) => onSaveClick(e)}>
 				<div>
 					<label>Nazwa</label>
-					<input></input>
+					<input onChange = {(e) => onNameChange(e)}></input>
 				</div>
 
 				<div>
 					<label>Opis</label>
-					<input></input>
+					<input onChange = {(e) => onDescriptionChange(e)}></input>
 				</div>
 
 				<div>
@@ -22,7 +44,7 @@ const TaskCreator = () =>
 						Dodaj
 					</button>
 
-					<button>
+					<button onClick = {() => onBackClick()}>
 						Powrót
 					</button>
 				</div>
