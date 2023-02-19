cb0d50f3369a4055c05917c95313f49c99d24f85
Jason <numberthirteen4026@gmail.com>
2023-02-19 15:33:52 +0100

    Add the tasks list as a route in the App.js script

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
 import './App.css';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
+
+import TasksList from "./components/tasksList.jsx"
 
 function App()
 {
 	return (
 		<div className="App">
 			<h1>ZADANIA DO WYKONANIA</h1>
+			<BrowserRouter>
+				<Routes>
+					<Route path = "/" element = {<TasksList />}></Route>
+				</Routes>
+			</BrowserRouter>
 		</div>
 	);
 }
