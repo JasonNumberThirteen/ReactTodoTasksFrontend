7d3d3e55b9a73e0a8a6ebce01e04a9aa24660bd9
Jason <numberthirteen4026@gmail.com>
2023-02-19 15:38:20 +0100

    Add basic structure to the tasks list

--- src/components/tasksList.jsx
+++ src/components/tasksList.jsx
@@ -6,6 +6,26 @@ const TasksList = () =>
 	return (
 		<div>
 			<h2>Lista zadań</h2>
+			<table className = "tasksList">
+				<thead className = "tasksListHead">
+					<tr>
+						<td>ID</td>
+						<td>Nazwa</td>
+						<td>Opis</td>
+						<td>Ukończone?</td>
+						<td>Edycja</td>
+					</tr>
+				</thead>
+				<tbody>
+
+				</tbody>
+			</table>
+			<button>
+				Wyświetl tylko ukończone zadania
+			</button>
+			<button>
+				Dodaj nowe zadanie
+			</button>
 		</div>
 	);
 };
